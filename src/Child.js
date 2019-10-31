4e5063bd6bbe731ab4fbef6de8e396a28ab248b6
Ann <thuyan.duong@flatironschool.com>
2019-10-30 22:32:08 -0400

    added solution

--- src/Child.js
+++ src/Child.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 class Child extends Component {
   render() {
     return (
-      <div
+      <div onClick={this.props.handleColorChange}
         className="child"
-        style={{backgroundColor: "#FFF"}}
+        style={{backgroundColor: this.props.color}}
       ></div>
     )
   }
--- src/Parent.js
+++ src/Parent.js
@@ -3,19 +3,26 @@ import { getRandomColor } from './randomColorGenerator.js'
 import Child from './Child'
 
 class Parent extends Component {
-
   constructor() {
     super()
     this.state = {
-      color: getRandomColor()
+      color: getRandomColor(),
+      childrenColor: '#FFF'
     }
   }
 
+  changeColor = () => {
+    this.setState({
+      color: getRandomColor(),
+      childrenColor: getRandomColor()
+    })
+  }
+  
   render() {
     return (
       <div className="parent" style={{backgroundColor: this.state.color}}>
-        <Child />
-        <Child />
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor}/>
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor}/>
       </div>
     )
   }
